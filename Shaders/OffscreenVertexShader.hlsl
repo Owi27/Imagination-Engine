4b6c3059736f992b587f6a8f9a034f438d4a5df2
Owi27 <owens.nicolas02@icloud.com>
2024-12-26 21:52:58 -0500

    fixed normalize

--- Shaders/OffscreenVertexShader.hlsl
+++ Shaders/OffscreenVertexShader.hlsl
@@ -23,7 +23,7 @@ VSOutput main(VSInput input)
 {
     VSOutput output;
     output.pos = mul(proj, mul(view, mul(world, float4(input.pos, 1))));
-    output.nrm = normalize(input.nrm);
+    output.nrm = -normalize(input.nrm);
     output.uv = input.uv;
     return output;
 }
