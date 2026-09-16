3cb8e27a7493b67112033d930704504ac0a183b1
Owens J. Nicolas <69972213+Owi27@users.noreply.github.com>
2026-09-15 22:15:38 -0400

    TAA P0: GBufferPC prevModel as float3x4 to fit 128-byte push constants

--- Imagination/src/Renderer/RenderPassStructs.h
+++ Imagination/src/Renderer/RenderPassStructs.h
@@ -5,8 +5,10 @@ namespace Imgn
 	struct GBufferPC
 	{
 		mat4 model;
-		mat4 prevModel;
 		uint32_t materialIndex = 0xFFFFFFFF;
+		uint32_t _pad0 = 0, _pad1 = 0, _pad2 = 0;
+		// float3x4 previous world matrix (fits push constants in 128 bytes with model+pads).
+		std::array<float, 12> prevModel{};
 	};
 
 	struct GBufferUBO
